69855da9ab1dc0bf9f4b90daac23b97708766fce
Ahmed Hassan <digiator2020@gmail.com>
2024-11-20 15:35:34 +0200

    chore: frontend - assign static files - added base html

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from bottle import Bottle, run, template
+from bottle import Bottle, run, template, static_file
 from config.settings import HOST, PORT, DEBUG
 from utils.main import get_template
 
 app = Bottle()
 
+@app.route('/static/<file:path>')
+def serve_static(file):
+    return static_file(file, root='./static')
+
 @app.route('/')
 def main():
-    return template(get_template("cmd_injection.html"), name="World")
+    return template(get_template("base.html"))
 
 if __name__ == "__main__":
     run(app, host=HOST, port=PORT, reloader=DEBUG)
--- utils/main.py
+++ utils/main.py
@@ -2,6 +2,18 @@
 
 
 def get_template(template_name, **kwargs):
+    """
+    Reads a template file and formats it with the provided keyword arguments.
+    Args:
+        template_name (str): The name of the template file to read.
+        **kwargs: Arbitrary keyword arguments to format the template.
+    Returns:
+        str: The formatted template string.
+    Raises:
+        FileNotFoundError: If the template file does not exist.
+        KeyError: If a placeholder in the template is not provided in kwargs.
+    """
+    
     with open(f"templates/{template_name}") as f:
         template = f.read()
     return template.format(**kwargs)
