a64fb6c2390e898cd3414760e6359d676087d753
Ahmed Hassan <digiator2020@gmail.com>
2024-11-20 19:58:40 +0200

    refactor: all routes in routes.py

--- app.py
+++ app.py
@@ -6,16 +6,5 @@ app = Bottle()
 
 add_routes(app)
 
-@app.route('/static/<file:path>')
-def serve_static(file):
-    return static_file(file, root='./static')
-
-@app.route('/')
-def main():
-    return template("main")
-
-# for route in app.routes:
-#     print(route)
-
 if __name__ == "__main__":
     run(app, host=HOST, port=PORT, debug=DEBUG, reloader=RELOADER)
--- config/routes.py
+++ config/routes.py
@@ -1,15 +1,23 @@
 from pathlib import Path
 from .settings import ROOT_DIR
-from bottle import template
+from bottle import template, template, static_file
 
 views_dir = Path(ROOT_DIR, 'views')
 
-def get_root_routes():
+def _serve_static(file):
+    return static_file(file, root='./static')
+
+def _main_view():
+    return template("main")
+
+def _get_root_routes():
     view_files = [f.name for f in views_dir.iterdir() if f.is_file()]
     routes = {file[:-4]: file for file in view_files if file.endswith('.tpl')}
     return routes
 
 def add_routes(app):
-    root_routes = get_root_routes()
+    app.route('/static/<file:path>', callback=_serve_static)
+    app.route('/', callback=_main_view)
+    root_routes = _get_root_routes()
     for route, view in root_routes.items():
         app.route(f'/{route}', method=["GET", "POST"])(lambda view=view: template(view, output='output'))
