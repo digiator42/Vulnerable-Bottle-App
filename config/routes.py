80e32c3a317006585119344d9f06f2e15517c353
Ahmed Hassan <digiator2020@gmail.com>
2024-11-21 16:19:01 +0200

    feat: dynamic trigger routes

--- config/routes.py
+++ config/routes.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from settings import ROOT_DIR
-from bottle import template, template, static_file
+from .settings import ROOT_DIR
+from bottle import template, template, static_file, request
 
 views_dir = Path(ROOT_DIR, 'views')
 triggers_dir = Path(ROOT_DIR, 'triggers')
@@ -13,12 +13,17 @@ def serve_static(file):
 def main_view():
     return template("_home")
 
+def _get_user_input(request):
+    if request.method == "GET":
+        return request.GET.get("user_input", "")
+    return request.POST.get("user_input", "")
+
 def _get_root_routes(ext: int=TPL_EXT):
     """
     Get all routes from the views directory
     `only` tpls which are not starting with underscore
     """
-    files = views_dir.iterdir() if ext == -4 else triggers_dir.iterdir()
+    files = views_dir.iterdir() if ext == TPL_EXT else triggers_dir.iterdir()
     view_files = [f.name for f in files if f.is_file()]
     routes = {file[:ext]: file for file in view_files if not file.startswith('_')}
     return routes
@@ -33,3 +38,11 @@ def add_routes(app):
         app.route(f'/{route}', method=["GET", "POST"])(
             lambda view=view: template(view, output='output')
         )
+    trigger_routes = _get_root_routes(PY_EXT)
+    for trigger, view in trigger_routes.items():
+        trigger_module = __import__(f'triggers.{trigger}', fromlist=['trigger'])
+        app.route(f'/trigger/{trigger}', method=["GET", "POST"])(
+            lambda trigger_module=trigger_module: trigger_module.trigger(_get_user_input(request))
+        )
+    for route in app.routes:
+        print(route.rule)
