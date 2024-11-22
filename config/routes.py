2575abd51a29c16b2815e14d13329e024dc96bd9
Ahmed Hassan <digiator2020@gmail.com>
2024-11-22 13:15:19 +0200

    refactor: add routes

--- config/routes.py
+++ config/routes.py
@@ -4,12 +4,10 @@ from pathlib import Path
 from .settings import ROOT_DIR
 from bottle import template, template, static_file, request
 
-views_dir = Path(ROOT_DIR, 'views')
-triggers_dir = Path(ROOT_DIR, 'triggers')
-PY_EXT = -3
-TPL_EXT = -4
+PY_EXT: int = -3
+TPL_EXT: int = -4
 
-def serve_static(file):
+def serve_static(file: str):
     return static_file(file, root='./static')
 
 def main_view():
@@ -25,6 +23,9 @@ def _get_routes(ext: int=TPL_EXT):
     Get all routes from the views directory
     `only` tpls which are not starting with underscore
     """
+    views_dir = Path(ROOT_DIR, 'views')
+    triggers_dir = Path(ROOT_DIR, 'triggers')
+    
     files = views_dir.iterdir() if ext == TPL_EXT else triggers_dir.iterdir()
     view_files = [f.name for f in files if f.is_file()]
     routes = {file[:ext]: file for file in view_files if not file.startswith('_')}
@@ -41,19 +42,13 @@ def get_trigger_functions(module):
         if trigger_pattern.match(name)
     }
 
-def add_routes(app):
-    app.route('/static/<file:path>', callback=serve_static)
-    app.route('/', callback=main_view)
-    
-    root_routes = _get_routes()
-
-    for route, view in root_routes.items():
-        app.route(f'/{route}', method=["GET", "POST"])(
-            lambda view=view: template(view, output='output')
-        )
-
+def add_trigger_routes(app):
+    """
+    Adds trigger routes., imports trigger modules and their functions,
+    then creates routes for each trigger function.
+    """
     trigger_routes = _get_routes(PY_EXT)
-    
+    # use view as tpl to return trigger result afterwords
     for trigger, view in trigger_routes.items():
         trigger_module = __import__(f'triggers.{trigger}', fromlist=['trigger'])
 
@@ -65,5 +60,20 @@ def add_routes(app):
             app.route(route_path, method=["GET", "POST"])(
                 lambda func=func: func(_get_user_input(request))
             )
-    # for route in app.routes:
-    #     print(route.rule)
+
+def add_root_routes(app):
+    """
+    Add routes for all views in the views directory.
+    """
+    root_routes = _get_routes()
+    for route, view in root_routes.items():
+        app.route(f'/{route}', method=["GET", "POST"])(
+            lambda view=view: template(view, output='output')
+        )    
+
+def add_routes(app):
+    app.route('/static/<file:path>', callback=serve_static)
+    app.route('/', callback=main_view)
+    
+    add_root_routes(app)
+    add_trigger_routes(app)
