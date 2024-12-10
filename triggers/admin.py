d85a52e3700c1843d6b4b74bb4426e948813b443
Ahmed Hassan <digiator2020@gmail.com>
2024-12-10 20:45:31 +0200

    refactor: admin brute force

--- triggers/admin.py
+++ triggers/admin.py
@@ -1,7 +1,7 @@
 import time
 from bottle import request, response
 from config.settings import MEDIUM_LEVEL, STRONG_LEVEL
-
+import math
 
 BANNED_IPS = {}
 ATTEMPS = {}
@@ -18,19 +18,21 @@ def trigger_admin(input):
     elif level == STRONG_LEVEL:
         return strong_admin(input)
     
-    if input['username'] == "admin" and input['password'] == "1234":
-        return "Access Granted!"
-    return "Access Denied!"
+    return _check_credentials(input)
 
 def check_ban(ip):
     if ip in BANNED_IPS:
+        # if withing the ban period, it's banned
         if time.time() < BANNED_IPS[ip]:
             return True
         else:
+            # remove the ban after duration time
             del BANNED_IPS[ip]
+            ATTEMPS[ip]['count'] = 1
     return False
 
-def ban_ip(ip, duration=60):
+def ban_ip(ip, duration=10):
+    # sets ban duraion to 5 mins
     BANNED_IPS[ip] = time.time() + duration
 
 def check_brute_force(ip):
@@ -62,23 +64,27 @@ def medium_admin(input):
         current_time = time.time()
         remaining_time = round(current_time - ATTEMPS[ip]['first_req_time'])
         response.status = 429
-        return f"Too many attempts. Try again later. {TIME_PERIOD - remaining_time} secs"
-    if input['username'] == "admin" and input['password'] == "1234":
-        return "Access Granted!"
-    return "Access Denied!"
+        return f"Too many attempts. Try again after {TIME_PERIOD - remaining_time} secs"
+
+    return _check_credentials(input)
 
 def strong_admin(input):
     ip = request.remote_addr
+    
+    # check wether the ip is banned or unlock the ban
     if check_ban(ip):
-        remaining_time = int((BANNED_IPS[ip] - time.time()) / 60) # blocked time in mins
-        return f"Too many attempts. Try again later. {remaining_time} mins"
+        remaining_time = math.ceil((BANNED_IPS[ip] - time.time()) / 60) # blocked time in mins
+        return f"Too many attempts. Try again after {remaining_time} mins"
 
     if check_brute_force(ip):
         current_time = time.time()
-        remaining_time = round(current_time - ATTEMPS[ip]['first_req_time'])
+        remaining_time = math.ceil(current_time - ATTEMPS[ip]['first_req_time'])
         response.status = 429
-        return f"Too many attempts. Try again later. {TIME_PERIOD - remaining_time} secs"
+        return f"Too many attempts. Try again after {TIME_PERIOD - remaining_time} secs"
     
+    return _check_credentials(input)
+
+def _check_credentials(input):
     if input['username'] == "admin" and input['password'] == "1234":
         return "Access Granted!"
     return "Access Denied!"
