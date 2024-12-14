524198a4e5695a4ef9cc49ae3414b0b584707145
Ahmed Hassan <digiator2020@gmail.com>
2024-12-14 02:48:06 +0200

    feat: directory traversal

--- triggers/directory-traversal.py
+++
@@ -1,3 +0,0 @@
-def trigger_directory_traversal(file_name):
-    with open(f"/var/www/{file_name}", "r") as file:
-        return file.read()
--- triggers/file-read.py
+++ triggers/file-read.py
@@ -1,6 +1,8 @@
 from typing import Dict
 from config.settings import MEDIUM_LEVEL, STRONG_LEVEL
 from bottle import request
+import os
+
 
 def trigger_file_read(input: Dict):
     level = request.environ.get('beaker.session')['level']
@@ -10,6 +12,9 @@ def trigger_file_read(input: Dict):
     if level == MEDIUM_LEVEL:
         return medium_file_read(input)
     
+    elif level == STRONG_LEVEL:
+        return strong_file_read(input)
+    
     try:
         with open(file_path, 'r') as file:
             content = file.read()
@@ -18,15 +23,19 @@ def trigger_file_read(input: Dict):
         return str(e)
 
 def medium_file_read(input: Dict):
-    
+   
     file_path = input['input']    
 
     try:
-        with open('./logs/' + file_path, 'r') as file:
+        with open('/var/www/' + file_path, 'r') as file:
             content = file.read()
         return content
     except Exception as e:
         return str(e)
     
 def strong_file_read(input: Dict):
-    pass
+    base_dir = '/var/www/'
+    requested_path = os.path.realpath(os.path.join(base_dir, input['input']))
+    
+    if not requested_path.startswith(base_dir):
+        return "Access denied: Invalid file path"
